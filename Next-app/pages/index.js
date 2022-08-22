fa8eab30fd50411741901b69d6823eb5b31f08d1
Hritick <hritickbhushan@gmail.com>
2022-08-22 20:07:51 +0530

    Added the files

---
+++ Next-app/pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+export default function App() {
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./images/favicon-32x32.png"
+        ></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="This is a site with navigation with dropdown"
+        />
+        <meta
+          name="keywords"
+          content="Next.js,react,javascript,SASS,SCSS,CSS,HTML-5,Progressive-Web-app,Navigation,dropdown"
+        />
+      </Head>
+      <main>Hello</main>
+    </>
+  );
+}
