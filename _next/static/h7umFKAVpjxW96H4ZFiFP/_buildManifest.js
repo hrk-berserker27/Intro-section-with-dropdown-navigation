2a8da51b853136c53d7f246c276e5edbba9e3a93
hrk-berserker27 <hritickbhushan@gmail.com>
2022-09-03 11:19:03 +0000

    Deploying to gh-pages from @ hrk-berserker27/Intro-section-with-dropdown-navigation@cb52d30436e1b271757d8c9daadabec177e69b92 🚀

---
+++ _next/static/h7umFKAVpjxW96H4ZFiFP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-af4525c7c7b5d40f.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
