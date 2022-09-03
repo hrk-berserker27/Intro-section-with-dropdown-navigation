69142ce0050ab52a4d76cb2daef724f80ce3783b
Hritick <hritickbhushan@gmail.com>
2022-09-03 17:16:30 +0530

    fixed the config file

--- next-config.js
+++ next-config.js
@@ -1,5 +1,7 @@
 /* @type {import('next').NextConfig}*/
 const nextConfig = {
   reactStricMode: true,
+  basePath: "/Intro-section-with-dropdown-navigation",
+  assetPrefix: "/Intro-section-with-dropdown-navigation",
 };
 export default nextConfig;
